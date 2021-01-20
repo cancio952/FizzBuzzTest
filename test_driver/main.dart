7541f20bc4abb71e1b290b9912c9c5869dfb3b0e
alejandro <acancio@acl.cl>
2021-01-20 12:22:54 -0500

    adding dependencies and main run BDD tests

---
+++ test_driver/main.dart
@@ -0,0 +1,8 @@
+import 'package:FizzBuzzTest/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+  runApp(MyApp());
+}
